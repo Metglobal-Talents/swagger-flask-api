470cdb9e64f4f2f0c23298a51e3fb1c42d67012d
Eyüp Arslan <eyup.arslan@metglobal.com>
2019-01-17 14:29:07 +0300

    app changed to connex_app

--- src/server.py
+++ src/server.py
@@ -4,13 +4,13 @@ Main module of the server file
 
 # 3rd party moudles
 import connexion
-from config import app
+from config import connex_app
 
 # create the application instance
 
 # Cead the swagger.yml file to configure the endpoints
-app.add_api("swagger.yml")
+connex_app.add_api("swagger.yml")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    connex_app.run(debug=True)
