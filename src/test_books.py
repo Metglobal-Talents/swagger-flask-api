c106f06310140da476951c6cd70157c941a723c5
Eyüp Arslan <eyup.arslan@metglobal.com>
2019-01-18 16:48:49 +0300

    some indent errors fixed

--- src/test_books.py
+++ src/test_books.py
@@ -117,8 +117,8 @@ class BookTestCase(unittest.TestCase):
                         "Content-Type": "application/json",
                         "Accept": "application/json"
                 })
-    status_code = rv.status_code
-    assert status_code == 400
+        status_code = rv.status_code
+        assert status_code == 400
 
     def test_update_book(self):
         data = {"book_name": "835 Satır", "author_name": "Nazım Hikmet",
@@ -131,7 +131,7 @@ class BookTestCase(unittest.TestCase):
         assert status_code == 200
 
     def test_update_not_found(self):
-        data = data = {"book_name": "It", "author_name": "Stephen King",
+        data = {"book_name": "It", "author_name": "Stephen King",
                        "genre": "Horror", "count": 100,
                        "publish_date": "1986", "ISBN": "asdf"}
         rv = self.app.put("/api/books",
@@ -141,7 +141,7 @@ class BookTestCase(unittest.TestCase):
         assert status_code == 404
 
     def test_update_missing_fields(self):
-        data = data = {"book_name": "It", "author_name": "Stephen King",
+        data = {"book_name": "It", "author_name": "Stephen King",
                        "genre": "Horror", "count": 100,
                        "publish_date": "1986"}
         rv = self.app.put("/api/books",
