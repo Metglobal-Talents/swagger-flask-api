f87f21011621e06e90e883fd10a7bee61f1a8dd3
Tolga Bilbey <tolga.bilbey@metglobal.com>
2019-01-18 08:31:08 +0300

    Return Endpoint

--- src/library.py
+++ src/library.py
@@ -42,5 +42,26 @@ def borrow_book():
 
 
 def return_book():
-    pass
+    json_data = request.get_json(force=True)
+    if not json_data:
+        abort(400, "Bad request")
+    
+    reservation_id = json_data.get("reservation_id")
+    reservation = Reservation.query.filter_by(
+                                    reservation_id=reservation_id).first()
+    if not reservation:
+        abort(404, "Reservation Not Found")
+    book_isbn = reservation.book_ISBN
+    book = Book.query.filter_by(ISBN=book_isbn).first()
+
+    if reservation.is_barrowed:
+        book.count += reservation.reservation_count
+    else:
+        book.reservation_count -= reservation.reservation_count
+    db.session.delete(reservation)
+    db.session.commit()
+
+    result = book_schema.dump(book).data
+    return result, 200
+
 
