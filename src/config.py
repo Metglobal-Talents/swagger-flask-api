9fbacbd4108cd72fe82b6b5cf531c8d5c5c0e87f
Tolga Bilbey <tolga.bilbey@metglobal.com>
2019-01-18 10:20:27 +0300

    db init values for swagger app

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
-import os
 import connexion
-from models import db
+import os
+import json
+from models import db, Book
 
 username = "postgres"
 password = ""
@@ -23,3 +24,29 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Create the SQLAlchemy db instance
 db.init_app(app)
 
+def read_json_file():
+    books_path = os.path.join(os.path.dirname(__file__), 'books.json')
+    with open(books_path, "r") as f:
+        data = f.read()
+    return data
+
+def init_db_values():
+    books = json.loads(read_json_file())
+    for book in books:
+        new_book = Book(
+            ISBN=book.get("ISBN"),
+            book_name=book.get("book_name"),
+            author_name=book.get("author_name"),
+            genre=book.get("genre"),
+            publish_date=book.get("publish_date"),
+            count=int(book.get("count")),
+            reservation_count=int(book.get("reservation_count"))
+        )
+        try:
+            db.session.add(new_book)
+            db.session.commit()
+        except:
+            pass
+
+with app.app_context():
+    init_db_values()
